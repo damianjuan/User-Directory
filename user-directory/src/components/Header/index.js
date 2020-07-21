da5189a2fcb9ba5489722a5fc1b9a3e57a5366c8
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-07-21 15:55:07 -0500

    added heaver component. currently functioning correctly. needs style added

---
+++ user-directory/src/components/Header/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    state = {
+        userInput: ""
+    };
+
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        let value = event.target.value;
+        const name = event.target.name;
+
+        //updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        event.preventDefault();
+        if (!this.state.userInput) {
+            alert("Enter a search term before submitting.");
+        } else {
+            alert("form submitted");
+            console.log(this.state.userInput);
+        }
+        this.setState({
+            userInput: ""
+        });
+    };
+
+
+    render() {
+        return (
+            <nav class="navbar navbar-light bg-light" >
+                <a class="navbar-brand">Employee Directory</a>
+                <form class="form-inline">
+                    <input
+                        class="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                        value={this.state.value}
+                        onChange={this.handleInputChange}
+                        name="userInput" />
+                    <button
+                        class="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                        onClick={this.handleFormSubmit}
+                    >Search</button>
+                </form>
+            </nav>)
+    }
+}
+
+export default Header;
