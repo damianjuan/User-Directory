fe817c3172cfcebf400aa0a0d8637573688c6573
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-07-22 19:45:27 -0500

    switch app js to class and switch to material ui from bootstrap for navbar

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Header from './components/Header/index';
+import SearchAppBar from './components/testHeader/index';
 
-function App() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+//class component to access input 
+class App extends Component {
+  state = {
+    userInput: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    //updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.userInput) {
+      alert("Enter a search term before submitting.");
+    } else {
+      alert("form submitted");
+      console.log(this.state.userInput);
+    }
+    this.setState({
+      userInput: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchAppBar />
+      </div >
+    );
+  }
 }
 
 export default App;
