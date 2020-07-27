9d1936adaab1830ffc2eb4ca4bfe335380bf2f25
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-07-26 23:49:05 -0500

    removed unused imports and made employees searchable by any field

--- user-directory/src/components/Table/index.js
+++ user-directory/src/components/Table/index.js
@@ -1,46 +1,99 @@
 import React, { Component } from 'react';
-import { TableContainer, Paper, Table, TableHead, TableCell, TableBody, TableRow, Avatar } from '@material-ui/core';
+import { TableContainer, Paper, Table, TableHead, TableCell, TableBody, TableRow, Avatar, TableSortLabel } from '@material-ui/core';
 import API from '../../utils/API'
 
-// //test data for testing table generation
-// function createData(name, number, email, location) {
-//     return { name, number, email, location };
-// }
-// //test data
-// const rows = [
-//     createData('Test One', 8165555555, 'one@email.com', 'kc'),
-//     createData('Test Two', 4095555555, 'two@email.com', 'galveston'),
-// ];
 
 class EmployeeTable extends Component {
     state = {
         results: [],
-        search: ""
+        userInput: ""
     };
-
     // when table component mounts get employees from API and assign to results state
     componentDidMount() {
         API.getUsers()
             .then(res => this.setState({ results: res.data.results }))
             .catch(err => console.log(err));
-
     }
+
+    handleInputChange = event => {
+        // Getting the value and name of the input which triggered the change
+        event.preventDefault();
+        let value = event.target.value;
+        const name = event.target.name;
+
+        //updating the input's state
+        this.setState({
+            [name]: value
+        });
+    };
+
+    handleFormSubmit = event => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        event.preventDefault();
+        if (!this.state.userInput) {
+            alert("Enter a search term before submitting.");
+        } else {
+            console.log(this.state.userInput);
+            const searchTerm = this.state.userInput;
+            const toFilter = this.state.results;
+            const regexp = new RegExp(searchTerm, 'i');
+            console.log(toFilter)
+            const filteredResults = toFilter.filter(el =>
+                regexp.test(el.name.first)
+                || regexp.test(el.name.last)
+                || regexp.test(el.name.last)
+                || regexp.test(el.phone)
+                || regexp.test(el.email)
+                || regexp.test(el.location.city)
+                || regexp.test(el.location.state)
+            );
+            // const filteredResults = toFilter.filter(function (el) {
+            //     return el.name.first === searchTerm
+            //         || el.name.last === searchTerm
+            //         || el.email === searchTerm
+            //         || el.phone === searchTerm
+            // });
+            console.log(filteredResults);
+            this.setState({ results: filteredResults })
+        }
+        this.setState({
+            userInput: ""
+        });
+    };
 
     render() {
         const rows = this.state.results;
         return (
 
+
+
             <TableContainer component={Paper}>
+                <form className="form-inline">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Search"
+                        aria-label="Search"
+                        value={this.state.value}
+                        onChange={this.handleInputChange}
+                        name="userInput" />
+                    <button
+                        className="btn btn-outline-success my-2 my-sm-0"
+                        type="submit"
+                        onClick={this.handleFormSubmit}
+                    >Search</button>
+                </form>
+
                 <Table>
                     <TableHead>
                         <TableRow>
                             <TableCell></TableCell>
-                            <TableCell align="right">Name</TableCell>
-                            <TableCell align="right">Phone Number</TableCell>
-                            <TableCell align="right">Email</TableCell>
-                            <TableCell align="right">Location</TableCell>
+                            <TableCell align="left"><TableSortLabel>Name</TableSortLabel></TableCell>
+                            <TableCell align="left">Phone Number</TableCell>
+                            <TableCell align="left">Email</TableCell>
+                            <TableCell align="left">Location</TableCell>
                         </TableRow>
-                    </TableHead>
+                    </TableHead >
                     <TableBody>
                         {rows.length ?
 
@@ -49,20 +102,21 @@ class EmployeeTable extends Component {
                                     <TableCell component="th" scope="rows">
                                         <Avatar alt={res.name.first} src={res.picture.thumbnail} />
                                     </TableCell>
-                                    <TableCell align="right">{res.name.first} {res.name.last}</TableCell>
-                                    <TableCell align="right">{res.phone}</TableCell>
-                                    <TableCell align="right">{res.email}</TableCell>
-                                    <TableCell align="right">{res.location.city},    {res.location.state}</TableCell>
+                                    <TableCell align="left">{res.name.first} {res.name.last}</TableCell>
+                                    <TableCell align="left">{res.phone}</TableCell>
+                                    <TableCell align="left">{res.email}</TableCell>
+                                    <TableCell align="left">{res.location.city},    {res.location.state}</TableCell>
                                 </TableRow>
                             ))
                             :
                             <TableRow>
-                                <TableCell align="center" >Loading</TableCell>
+                                <TableCell align="center" colSpan={5}>Loading...</TableCell>
                             </TableRow>
                         }
                     </TableBody>
-                </Table>
-            </TableContainer>
+                </Table >
+            </TableContainer >
+
         )
     }
 };
--- user-directory/src/components/testHeader/index.js
+++ user-directory/src/components/testHeader/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-//import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-//import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -82,7 +79,7 @@ export default function SearchAppBar({ state, handleInputChange, handleFormSubmi
                     <Typography className={classes.title} variant="h6" noWrap>
                         Employee Directory
           </Typography>
-                    <div className={classes.search}>
+                    {/* <div className={classes.search}>
                         <div className={classes.searchIcon}>
                             <SearchIcon />
                         </div>
@@ -95,7 +92,7 @@ export default function SearchAppBar({ state, handleInputChange, handleFormSubmi
                             inputProps={{ 'aria-label': 'search' }}
                             onChange={handleInputChange}
                         />
-                    </div>
+                    </div> */}
                 </Toolbar>
             </AppBar>
         </div>
